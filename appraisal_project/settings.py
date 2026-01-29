0a52598dd9dbb317a58aef8d6cf4508b0a71f1b5
iyadangky <iyadangky@naver.com>
2026-01-29 22:57:53 +0900

    리뉴얼

--- appraisal_project/settings.py
+++ appraisal_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&1@#7y)@%%3$dj!()ztr)=94wkk995hn*#cewy-xt@#qux(gbm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # For AWS deployment (Restrict this to IP/Domain later)
 
